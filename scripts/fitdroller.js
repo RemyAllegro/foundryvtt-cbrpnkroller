88307a6d13697435511c602a602afba587915e2f
Remy Allegro <40637753+RemyAllegro@users.noreply.github.com>
2022-09-16 18:17:53 -0400

    threat/effect initial commit

--- scripts/fitdroller.js
+++ scripts/fitdroller.js
@@ -323,38 +323,35 @@ export default class Roller {
 
     let roll_status = "failure";
 
-    if (sorted_rolls[0] === 6 && zeromode){
-      roll_status = "critical-success";
+    
+    let use_die;
+    let prev_use_die = false;
+
+    if (zeromode){
+      use_die = sorted_rolls[0];
     } else {
-      let use_die;
-      let prev_use_die = false;
+      use_die = sorted_rolls[sorted_rolls.length - 1];
 
-      if (zeromode){
-        use_die = sorted_rolls[0];
-      } else {
-        use_die = sorted_rolls[sorted_rolls.length - 1];
-
-        if (sorted_rolls.length - 2 >= 0){
-          prev_use_die = sorted_rolls[sorted_rolls.length - 2];
-        }
+      if (sorted_rolls.length - 2 >= 0){
+        prev_use_die = sorted_rolls[sorted_rolls.length - 2];
       }
+    }
 
-      // 1,2,3 = failure
-      if (use_die <= 3){
-        roll_status = "failure";
-        // if 6 - check the prev highest one.
-      } else if (use_die === 6){
-        // 6,6 - critical success
-        if (prev_use_die && prev_use_die === 6){
-          roll_status = "critical-success";
-        } else {
-          // 6 - success
-          roll_status = "success";
-        }
+    // 1,2,3 = failure
+    if (use_die <= 3){
+      roll_status = "failure";
+      // if 6 - check the prev highest one.
+    } else if (use_die === 6){
+      // 6,6 - critical success
+      if (prev_use_die && prev_use_die === 6){
+        roll_status = "critical-success";
       } else {
+        // 6 - success
+        roll_status = "success";
+      }
+    } else {
         // else (4,5) = partial success
         roll_status = "partial-success";
-      }
     }
     return roll_status;
   }
